64271a02f639b0ec79c86b576b9e01f1639e5719
lawang24 <catasleen6@gmail.com>
2022-12-25 15:17:24 -0500

    Basic game functions work

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,163 @@
-import { useState, useEffect } from "react";
 import Chessboard from "chessboardjsx";
 import io from 'socket.io-client';
+import { Chess } from "chess.js";
+import { Component } from "react";
+import PropTypes from "prop-types";
 
 
-
-// const [message, setMessage] = useState("");
-// const [messageReceived, setMessageReceived] = useState([]);
-
-// const sendMessage = () => {
-//   socket.emit("send_message", { message });
-//   setMessage("");
-// };
-
-// useEffect(() => {
-//   socket.on("receive_message", (data) => {
-//     setMessageReceived(data);
-//   });
-// }, [socket])
-
+const STOCKFISH = window.STOCKFISH;
 const socket = io.connect("http://localhost:3001");
+const game = new Chess();
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fen: "start",
+      turn: true,
+      team: "none"
+    };
+    this.sendRating = this.sendRating.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+    this.joinTeam = this.joinTeam.bind(this);
+  };
 
-  const [position, setPosition] = useState("start");
+  componentDidMount() {
+    socket.on("weakest_position", (weakest) => {
+      this.setState({ fen: weakest });
+      game.load(weakest);
+    });
+    socket.on("play", () => {
+      this.setState({ turn: true });
+    });
+  };
 
-  const sendMove = (onDrop) => {
-    socket.emit("move", onDrop)
-    console.log(onDrop);
+  sendRating = (rating, position) => {
+    socket.emit("player_move", ({ rating: rating, position: position }));
+    this.setState({ turn: false });
+  };
+
+  joinTeam = (team) => {
+    socket.emit("join_team", team);
+    this.setState({ team: team });
   }
 
-  useEffect(() => {
-    socket.on("receive_move", (move) => {
-      if (move === null) return;
-      setPosition(move);
-    });
-  }, [socket])
+  onDrop = ({ sourceSquare, targetSquare }) => {
+    if (!this.state.turn) return;
+    const move = game.move({ from: sourceSquare, to: targetSquare })
+    if (move === null) return; // illegal move  
 
-  return (
-    <div style={boardsContainer}>
-      <Chessboard
-        id="board!"
-        position={position}
-        width={320}
-        onDrop={sendMove}
-        boardStyle={boardStyle}
-        orientation="white"
-      />
-    </div>
-  );
+    // grab player proposed position and show it
+    this.setState({ fen: game.fen() })
+
+    game.undo();
+
+    return new Promise((resolve) => {
+      resolve();
+    }).then(() => this.engineGame().evalMove());
+  };
+
+  engineGame = (options) => {
+    options = options || {};
+
+    /// We can load Stockfish via Web Workers or via STOCKFISH() if loaded from a <script> tag.
+    let engine =
+      typeof STOCKFISH === "function"
+        ? STOCKFISH()
+        : new Worker(options.stockfishjs || "stockfish.js");
+    let announced_game_over;
+
+    setInterval(() => {
+      if (announced_game_over) {
+        return;
+      }
+      if (game.isGameOver()) {
+        announced_game_over = true;
+      }
+    }, 500);
+
+    const evalMove = () => {
+      if (!game.isGameOver()) {
+        engine.postMessage("ucinewgame");
+        engine.postMessage("position fen " + this.state.fen);
+        engine.postMessage("eval");
+      }
+    };
+
+    engine.onmessage = (event) => {
+      let line;
+
+      if (event && typeof event === "object") {
+        line = event.data;
+      } else {
+        line = event;
+      }
+      if (line.substr(0, "Total Evaluation".length) === "Total Evaluation") {
+        let score = parseFloat(line.substr(18).substr(0, 4));
+        if (this.state.team === "black") score = score * -1;
+        this.sendRating(score, this.state.fen);
+      }
+    };
+
+    return {
+      start: function () {
+        engine.postMessage("ucinewgame");
+        engine.postMessage("isready");
+        announced_game_over = false;
+      },
+      evalMove: function () {
+        evalMove();
+      },
+    };
+  };
+
+  // useEffect(() => {
+  //   socket.on("receive_move", (move) => {
+  //     if (move === null) return;
+  //     setPosition(move);
+  //   });
+  // }, [socket])
+
+  render() {
+
+    let status = (this.state.turn ? "Your" : "Not Your")
+   
+    let team;
+    switch (this.state.team){
+      case "white":
+        team = "White"
+        break;
+      case "black":
+        team = "Black"
+        break;
+      default :
+        team = "Spectating"
+    }
+
+    return (
+      <div style={boardsContainer} >
+        <Chessboard
+          id="board!"
+          position={this.state.fen}
+          width={320}
+          onDrop={this.onDrop}
+          boardStyle={boardStyle}
+          orientation="white"
+        />
+        <button value="white" onClick={e => this.joinTeam(e.target.value, true)} >Join White Team</button>
+        <button value="black" onClick={e => this.joinTeam(e.target.value, false)} >Join Black Team</button>
+        <h1>{team} Player</h1>
+        <h1>{status} Turn </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
 
 const boardsContainer = {
   display: "flex",
-  justifyContent: "space-around",
+  justifyContent: "space-evenly",
   alignItems: "center"
 };
 const boardStyle = {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
-
 // setting up the server
 const express = require('express');
 const app = express();
 const http = require("http");
 const { Server } = require('socket.io');
 const cors = require("cors");
+const { createDiffieHellmanGroup } = require('crypto');
 app.use(cors());
 const server = http.createServer(app)
+
+var moves = [];
+let numberPlayers = 2;
+let whiteTurn = true ;
+
+function chooseWeakest(moves) {
+    let index = 0;
+    let champ = moves[0].rating;
+    for (let i = 1; i < moves.length; i++) {
+        if (moves[i].rating < champ) {
+            champ = moves[i].rating;
+            index = i;
+    }
+    return moves[index].position;
+}
+}
 
 const io = new Server(server, {
     cors: {
@@ -15,21 +31,37 @@ const io = new Server(server, {
     }
 });
 
-// setting up the chess game
-const {Chess} = require ('chess.js');
-const game = new Chess();
+const turnIsOver = () =>{
+    moves = [];
+    whiteTurn = !whiteTurn;
+    console.log(whiteTurn);
+    if (whiteTurn) io.to("white").emit("play");
+    if (!whiteTurn) io.to("black").emit("play");
+}
 
 io.on("connection", (socket) => {
     console.log('User Connected: ' + socket.id);
-    socket.on("move", (onDrop) => {
-        const isValid = game.move({ from: onDrop.sourceSquare, to: onDrop.targetSquare });
-        let newPosition = isValid ? game.fen() : null;
-        io.emit("receive_move", newPosition);
+    
+    socket.on("player_move", (rating) => {
+        moves.push(rating);
+        console.log(moves);
+        if (numberPlayers != 0 && moves.length == numberPlayers) {
+            let weakest = chooseWeakest(moves);
+            io.emit("weakest_position", weakest);
+            turnIsOver();
+            console.log(moves);
+        }
     });
+
+    socket.on("join_team", (data) =>{
+        socket.join(data);
+    });
+
 });
 
 server.listen(3001, () => {
     console.log("server is running")
 });
+
 
 
